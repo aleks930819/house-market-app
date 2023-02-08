e314e7b13fa1b86ec4f5c1813a4b0b7256c9fbe0
Aleksandar Atanasov <107752460+aleks930819@users.noreply.github.com>
2023-02-08 21:56:57 +0200

    added profile card

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-import { AiFillMessage } from 'react-icons/ai';
-
-import { getAuth, updateProfile } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import {
   updateDoc,
   doc,
@@ -25,19 +23,17 @@ import Button from '../components/Button';
 import Modal from '../components/Modal';
 import MyProperties from '../components/MyProperties';
 
-import DefaultProfilePhoto from '../assets/images/profile.jpg';
+import ProfileCard from '../components/ProfileCard';
 
 const Profile = () => {
   const auth = getAuth();
 
   const userId = auth.currentUser.uid;
 
-  console.log(userId);
 
   const [loading, setLoading] = useState(false);
   const [properties, setProperties] = useState(null);
   const [filteredProperties, setFilteredProperties] = useState([]);
-  const [messages, setMessages] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [itemToDeleteId, setItemToDeleteId] = useState(null);
 
@@ -64,10 +60,6 @@ const Profile = () => {
 
     fetchUserListings();
   }, [auth.currentUser.uid]);
-
-  if (loading) {
-    return <Spinner />;
-  }
 
   const onDeleteHandler = async () => {
     try {
@@ -111,39 +103,14 @@ const Profile = () => {
     </Modal>
   );
 
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <Container>
-        <div className="mt-10 text-xs sm:text-md  border shadow-md p-5 flex flex-col gap-2 rounded-md ">
-          <div className="grid grid-cols-2 place-items-center">
-            <div className="w-16 h-16">
-              <img
-                src={auth.currentUser.photoURL || DefaultProfilePhoto}
-                className="w-full h-full object-cover rounded-full"
-              />
-            </div>
-            <Link to="/messages">
-              <div className="flex items-center gap-2 cursor-pointer">
-                <AiFillMessage className="text-2xl place-items-center text-cyan-900" />
-                <h2>My Messages</h2>
-              </div>
-            </Link>
-          </div>
-
-          <div className="flex flex-col items-center mt-5">
-            <h1 className="text-sm font-bold">
-              {auth.currentUser.displayName}
-            </h1>
-            <p>{auth.currentUser.email}</p>
-          </div>
-          <div className="flex  gap-2 mt-5">
-            <Button to={`/edit/${userId}`} primary>
-              Edit Profile
-            </Button>
-            <Button danger>Delete Profile</Button>
-          </div>
-        </div>
-
+        <ProfileCard />
         <MyProperties
           filteredProperties={filteredProperties}
           handleModalView={handleModalView}
